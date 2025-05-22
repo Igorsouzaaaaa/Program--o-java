272dd9d82f797bda803d2b831a05ca5e149044b8
igor <igorsouza1701@outlook.com>
2025-05-22 20:33:00 -0300

    exercicios

---
+++ exercicio 4/js/script.js
@@ -0,0 +1,17 @@
+function exce4(){
+      let numero = Number(prompt(`Informe um número ${conta}`))
+      for (let conta = 0; conta <= 10; conta ++){
+      console.log((`${numero} x ${conta} = ${numero * conta}`))
+    }
+}
+
+
+// exercicio 5 
+function exce5(){
+    for (let conta1 = 0; conta1 <= 10; conta1 ++) {
+        for (let conta2 = 1; conta2 <= 10; conta2 ++){
+        } 
+        console.log((` ${conta1} x ${conta2} = ${conta1 * conta2}`))
+    }
+}
+// execercicio 6
